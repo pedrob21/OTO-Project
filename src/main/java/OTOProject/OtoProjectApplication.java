a47208df4fca9c963e7bdb51ee490566b6ccfd63
danielegallego <danielegallegoo@gmail.com>
2026-09-21 23:07:40 -0300

    feat: estrutura base do projeto

---
+++ src/main/java/OTOProject/OtoProjectApplication.java
@@ -0,0 +1,13 @@
+package OTOProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/OTOProject/OtoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package OTOProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
